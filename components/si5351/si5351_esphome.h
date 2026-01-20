f3bf621485f938f32bb008a41ac8fcc5ef378641
Vincent <96040709+Jasionf@users.noreply.github.com>
2026-01-20 17:41:24 +0800

    Update si5351_esphome.h

--- components/si5351/si5351_esphome.h
+++ components/si5351/si5351_esphome.h
@@ -2,32 +2,36 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/i2c/i2c.h"
+#include "esphome/core/gpio.h"
+#include "esphome/core/hal.h"
+#include "esphome/core/log.h"
 
-static const uint8_t SI5351_I2C_ADDR = 0x60;
-  // Register addresses
-  static const uint8_t REG_OUTPUT_ENABLE_CONTROL = 3;
-  static const uint8_t REG_OEB_PIN_ENABLE_CONTROL = 9;
-  static const uint8_t REG_OUTPUT_DRIVERS = 16;
-  static const uint8_t REG_CLK1_CONTROL = 17;
-  static const uint8_t REG_XTAL_LOAD_CAP = 183;
-  static const uint8_t REG_MULTISYNTH_NA = 26;
-  static const uint8_t REG_MULTISYNTH1 = 50;
-  static const uint8_t REG_PLL_RESET = 177;
-
-  
 namespace esphome {
 namespace si5351 {
 
-class Si5351Component : public Component, public i2c::I2CDevice {
+static const uint8_t SI5351_I2C_ADDR = 0x60;
+// Register addresses
+static const uint8_t REG_OUTPUT_ENABLE_CONTROL = 3;
+static const uint8_t REG_OEB_PIN_ENABLE_CONTROL = 9;
+static const uint8_t REG_OUTPUT_DRIVERS = 16;
+static const uint8_t REG_CLK1_CONTROL = 17;
+static const uint8_t REG_XTAL_LOAD_CAP = 183;
+static const uint8_t REG_MULTISYNTH_NA = 26;
+static const uint8_t REG_MULTISYNTH1 = 50;
+static const uint8_t REG_PLL_RESET = 177;
+
+class Si5351Component : public esphome::Component, public esphome::i2c::I2CDevice {
  public:
   void setup() override;
   void dump_config() override;
-  float get_setup_priority() const override { return setup_priority::PRE_HARDWARE; }
+  float get_setup_priority() const override { return esphome::setup_priority::HARDWARE; }
 
   bool write_reg(uint8_t reg, uint8_t value);
   bool read_reg(uint8_t reg, uint8_t *out_value);
   bool apply_table(const uint8_t *pairs, size_t length);
 
+ protected:
+  esphome::GPIOPin *reset_gpio_{nullptr};
 };
 
 }  // namespace si5351
