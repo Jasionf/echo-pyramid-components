297699702816ef8d184bd902620033fb9eca4afe
Vincent <96040709+Jasionf@users.noreply.github.com>
2026-01-20 15:33:40 +0800

    Update __init__.py

--- components/aw87559/__init__.py
+++ components/aw87559/__init__.py
@@ -2,35 +2,18 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c
 from esphome.const import CONF_ID, CONF_RESET_PIN
-from esphome import pins
 
-CODEOWNERS = ["@Vincent"]
 DEPENDENCIES = ["i2c"]
-AUTO_LOAD = []
+CODEOWNERS = ["@Jasionf"]
 
 aw87559_ns = cg.esphome_ns.namespace("aw87559")
 AW87559Component = aw87559_ns.class_("AW87559Component", cg.Component, i2c.I2CDevice)
 
-CONF_AW87559_ID = "aw87559_id"
-
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(AW87559Component),
-            cv.Optional(CONF_RESET_PIN): pins.gpio_pin_schema,
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
-    .extend(i2c.i2c_device_schema(0x58))  # Default I2C address 0x58
-)
-
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[cv.GenerateID()])
-    await cg.register_component(var, config)
-    await i2c.register_i2c_device(var, config)
-    
-    # Configure reset GPIO if provided
-    if CONF_RESET_PIN in config:
-        reset_pin = await cg.gpio_pin_digital_output_pin_expression(config[CONF_RESET_PIN])
-        cg.add(var.set_reset_gpio(reset_pin))
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(AW87559Component),
+        cv.Optional(CONF_RESET_PIN): cv.All(
+            cv.only_on_esp32, cv.only_on_esp8266, cv.invalid("Reset pin is not supported on this platform")
+        ),
+    }
+).extend(cv.COMPONENT_SCHEMA).extend(i2c.i2c_device_schema(0x58))  # 你的地址是 0x58
